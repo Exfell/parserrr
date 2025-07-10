e81dae7793b996584ec99c16e8ec0b9099672f6c
Exfell <100291975+Exfell@users.noreply.github.com>
2025-07-10 17:00:00 +0500

    Update divide.py

--- divide.py
+++ divide.py
@@ -1,12 +1,17 @@
 import csv
+import logging
 
+
+logger = logging.getLogger(__name__)
 # Путь к исходному файлу
 def divide(input_file):
     # Считываем все строки
     with open(input_file, 'r', encoding='utf-8-sig', errors='replace') as f:
         reader = list(csv.reader(f))
         total_rows = len(reader)
-    n = 3 # т.к. 3 процесса
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    logger.debug(f"Всего строк: {total_rows}")
+    n = 3  # т.к. 3 процесса
     # Вычисляем размер одной части
     chunk_size = total_rows // n
     remainder = total_rows % n  # остаток, который нужно распределить
@@ -15,7 +20,7 @@ def divide(input_file):
         # Распределяем остаток по частям
         end = start + chunk_size + (1 if i < remainder else 0)
         chunk = reader[start:end]
-    
+
         # Записываем чанк в новый файл
         output_file = f'{i + 1}.csv'
         with open(output_file, 'w', newline='', encoding='utf-8-sig', errors='replace') as f:
@@ -23,6 +28,8 @@ def divide(input_file):
             writer.writerows(chunk)
         # Обновляем стартовую позицию для следующего чанка
         start = end
+
+
 if __name__ == '__main__':
-    #divide()
+    # divide()
     pass

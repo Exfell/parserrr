1711948f6aa30f65c857765e02b83b3b788f9584
Exfell <100291975+Exfell@users.noreply.github.com>
2025-07-14 21:11:45 +0500

    Update collect.py

--- collect.py
+++ collect.py
@@ -6,11 +6,11 @@ def collect():
     files = ['out_1.csv', 'out_2.csv', 'out_3.csv']
     files2 = ['1.csv','2.csv','3.csv']
     # Чтение и объединение
-    dfs = [pd.read_csv(file, header=None,encoding='utf-8-sig',delimiter=';') for file in files]
+    dfs = [pd.read_csv(file, header=None,encoding='utf-8-sig',delimiter=',') for file in files]
     merged_df = pd.concat(dfs, ignore_index=True)
 
     # Сохраняем итоговый файл
-    merged_df.to_csv('merged.csv', index=False, header=False, encoding='utf-8-sig',sep=';')
+    merged_df.to_csv('merged.csv', index=False, header=False, encoding='utf-8-sig',sep=',')
 
     # Удаляем старые файлы
     for file in files:
